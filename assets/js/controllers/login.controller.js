f6caf9cebda5d85fe8b5b8d08cd2131f8cea7e16
20203tn125 <20203tn125@utez.edu.mx>
2023-12-04 20:58:53 -0600

    [UPDATE] Funcionalidad de registro completada

--- assets/js/controllers/login.controller.js
+++ assets/js/controllers/login.controller.js
@@ -3,45 +3,49 @@ app.controller('LOGIN_CONTROLLER', ['$scope', '$http', ($scope, $http) => {
         $scope.switchOperation();
     });
 
+    $scope.register = {}
     $scope.sessionActive = false;
-    $scope.users = ['sergiocortes518@gmail.com',
-     'alcocer32@gmail.com',
-     'rodolfo21@gmail.com']
+    $scope.users = [
+        {
+            name: 'Sergio',
+            surname: 'Cortes',
+            lastname: 'Popoca',
+            password: "admin",
+            role: "Cliente",
+            email: 'sergiocortes518@gmail.com',
+            birthdate: formatDate(new Date('2000-12-09')),
+            address: 'Col.Altapalmira, Av.Insurgentes #6 ',
+            references: 'Col.Rivera, Av.Portillo #4',
+            phone: '(+52)7779876543',
+            hotel: {
+                name: 'Labored',
+                colors: [
+                    '#212529', '#59367d', '#6FFD73'
+                ]
+            }
+        },
+        {
+            name: 'Alcocer',
+            surname: 'Dominguez',
+            lastname: 'Popoca',
+            email:'alcocer32@gmail.com',
+            password: "admin",
+            role:"Proveedor",
+            birthdate: formatDate(new Date('2000-12-09')),
+            address: 'Col.Altapalmira, Av.Insurgentes #6 ',
+            references: 'Col.Rivera, Av.Portillo #4',
+            phone: '(+52)7779876543',
+            hotel: {
+                name: 'Labored',
+                colors: [
+                    '#212529', '#59367d', '#6FFD73'
+                ]
+            }
+        }
+
+    ]
 $scope.user = {}
 
-$scope.user1 = {
-    name: 'Sergio',
-    surname: 'Cortes',
-    lastname: 'Popoca',
-    email: 'sergiocortes518@gmail.com',
-    birthdate: formatDate(new Date('2000-12-09')),
-    address: 'Col.Altapalmira, Av.Insurgentes #6 ',
-    references: 'Col.Rivera, Av.Portillo #4',
-    phone: '(+52)7779876543',
-    hotel: {
-        name: 'Labored',
-        colors: [
-            '#212529', '#59367d', '#6FFD73'
-        ]
-    }
-}
-//`${new Date('2000/09/12').getDate().toString().padStart(2,'0')}-${(new Date('2000/09/12').getMonth() + 1).toString().padStart(2, '0')}-${new Date('2000/09/12').getFullYear()}`
-    $scope.user2 = {
-        name: 'Alcocer',
-        surname: 'Dominguez',
-        lastname: 'Popoca',
-        email:'alcocer32@gmail.com',
-        birthdate: formatDate(new Date('2000-12-09')),
-        address: 'Col.Altapalmira, Av.Insurgentes #6 ',
-        references: 'Col.Rivera, Av.Portillo #4',
-        phone: '(+52)7779876543',
-        hotel: {
-            name: 'Labored',
-            colors: [
-                '#212529', '#59367d', '#6FFD73'
-            ]
-        }
-    }
 
     function formatDate(date) {
         const year = date.getFullYear();
@@ -62,29 +66,91 @@ $scope.user1 = {
     } 
 
     const login = () => {
-        // TODO del login
-       if($scope.user.name == 'sergiocortes518@gmail.com'){
-        $scope.user = $scope.user1;
-        $scope.user.role = 'Cliente';
-       }else if($scope.user.name == 'alcocer32@gmail.com'){
-        $scope.user = $scope.user2;
-        $scope.user.role = 'Proveedor';
-       }else{
-        alert('Usuario no existente')
-       }
-       localStorage.setItem('role', $scope.user.role);
-       localStorage.setItem('user', $scope.user.email);
-       localStorage.setItem('infUser', JSON.stringify($scope.user));
-       localStorage.setItem('hotelName', $scope.user.hotel.name);
-       localStorage.setItem('hotelColors', $scope.user.hotel.colors);
-       localStorage.setItem('page', 0);
+
        
-    window.location.replace('../../../view/auth/index.html');
+        console.log($scope.users.findIndex(us => us.email == $scope.user.email ))
+        console.log($scope.users)
+        // TODO del login
+       if($scope.users.find(us => us.email == $scope.user.email ) != undefined){
+        let findUsIndex = $scope.users.findIndex(us => us.email == $scope.user.email );
+        if($scope.user.password == $scope.users[findUsIndex].password){
+            $scope.user =$scope.users[findUsIndex];
+            Toastify({
+                text: "Bienvenido " + `${$scope.user.name} ${$scope.user.surname} ${$scope.user.lastname}` ,
+                gravity: "top",
+                position: 'right',
+                duration: 1700,
+                className: 'success',
+                close: true,
+                style: {
+                  background: "linear-gradient(to right, #44C862, #6EFF8F)",
+                }
+              }).showToast();
+              localStorage.setItem('role', $scope.user.role);
+              localStorage.setItem('user', $scope.user.email);
+              localStorage.setItem('infUser', JSON.stringify($scope.user));
+              localStorage.setItem('hotelName', $scope.user.hotel.name);
+              localStorage.setItem('hotelColors', $scope.user.hotel.colors);
+              localStorage.setItem('page', 0);
+              
+           window.location.replace('../../../view/auth/index.html');
+        }else{
+            Toastify({
+                text: "Correo/Contraseña incorrectos",
+                gravity: "top",
+                position: 'right',
+                close: true,
+                style: {
+                  background: "linear-gradient(to right, #C8770F, #FFB250)",
+                }
+              }).showToast();    
+        }
+        
+       }else{
+        Toastify({
+            text: "Usuario no existente",
+            gravity: "top",
+            position: 'right',
+            close: true,
+            style: {
+              background: "linear-gradient(to right, #C8770F, #FFB250)",
+            }
+          }).showToast();
+       }
+   
      
         
     }
 
-  
+    const register = () => {
+        
+    }
 
-    $scope.switchOperation = () => $scope.form ? login() : register();
+    $scope.registrar = (user) =>{
+        user.birthdate = ''
+        user.references = ''
+        user.hotel = {
+            name: 'Labored',
+            colors: [
+                '#212529', '#59367d', '#6FFD73'
+            ]
+        }
+
+        $scope.users.push(user);
+        console.log($scope.users)
+        Toastify({
+            text: "Usuario registrado, ya puedes iniciar sesión",
+            gravity: "top",
+            position: 'right',
+            duration: 1700,
+            className: 'success',
+            close: true,
+            style: {
+              background: "linear-gradient(to right, #44C862, #6EFF8F)",
+            }
+          }).showToast();
+          $scope.changeForm();
+    }
+
+    $scope.switchOperation = () => $scope.form ? login() : $scope.registrar();
 }]);
